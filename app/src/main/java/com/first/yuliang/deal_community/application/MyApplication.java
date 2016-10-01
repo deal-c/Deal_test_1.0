bf1f9585cdc00b6d2db50edd94f746b36e9051f5
deal-c <2598804481@qq.com>
2016-10-01 11:10:21 +0800

    auther:张煜 text:登录注册 text2:我的模块的所有内容

--- app/src/main/java/com/first/yuliang/deal_community/application/MyApplication.java
+++ app/src/main/java/com/first/yuliang/deal_community/application/MyApplication.java
@@ -2,6 +2,8 @@ package com.first.yuliang.deal_community.application;
 
 import android.app.Application;
 
+import com.first.yuliang.deal_community.pojo.User;
+
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
