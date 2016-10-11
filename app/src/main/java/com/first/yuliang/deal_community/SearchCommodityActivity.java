292ab3fdbd73c569bb0b307a4c37b177880df96d
deal-c <2598804481@qq.com>
2016-10-11 21:59:44 +0800

    author:sun ---------------- desc:跳转search界面

--- app/src/main/java/com/first/yuliang/deal_community/SearchCommodityActivity.java
+++ app/src/main/java/com/first/yuliang/deal_community/SearchCommodityActivity.java
@@ -1,13 +1,66 @@
 package com.first.yuliang.deal_community;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SearchCommodityActivity extends AppCompatActivity {
+
+    private EditText query2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_commodity);
+
+        query2 = ((EditText) findViewById(R.id.query2));
+        query2.requestFocus();
+    }
+
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            // 获得当前得到焦点的View，一般情况下就是EditText（特殊情况就是轨迹求或者实体案件会移动焦点）
+            View view = getCurrentFocus();
+            if (isHideInput(view, ev)) {
+                hideSoftInput(view);
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    // 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时没必要隐藏
+    private boolean isHideInput(View v, MotionEvent ev) {
+        int[] l = {0, 0};
+        v.getLocationInWindow(l);
+        int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left
+                + v.getWidth();
+        if (v != null && (v instanceof EditText)) {
+
+            if (ev.getX() > left && ev.getX() < right && ev.getY() > top
+                    && ev.getY() < bottom) {
+                // 点击EditText的事件，忽略它。
+                return false;
+            } else {
+                return true;
+            }
+        }
+        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditView上，和用户用轨迹球选择其他的焦点
+        return false;
+    }
+
+    // 隐藏软键盘
+    private void hideSoftInput(View view) {
+        query2.setFocusable(false);
+        query2.setFocusableInTouchMode(true);
+        InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (manager != null) {
+            manager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
