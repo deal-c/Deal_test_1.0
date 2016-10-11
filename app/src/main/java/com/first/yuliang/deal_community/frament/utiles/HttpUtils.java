ad3fbab243d0000d998f6f33a2754c9ed21f12ec
deal-c <2598804481@qq.com>
2016-10-11 11:36:38 +0800

    Modifyer:张煜 text:分类模块

---
+++ app/src/main/java/com/first/yuliang/deal_community/frament/utiles/HttpUtils.java
@@ -0,0 +1,9 @@
+package com.first.yuliang.deal_community.frament.utiles;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public class HttpUtils {
+    public static final String host="http://10.40.5.52:8080/FourProject/";
+    public static final String host1="http://10.40.5.52:8080";
+}
