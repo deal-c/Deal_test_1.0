ae740ce943d0b28f6d2f95cb2d2be1459867714d
deal-c <2598804481@qq.com>
2016-10-13 16:48:19 +0800

    author:  yuliang   desc:    修复fragment切换的问题

--- app/src/main/java/com/first/yuliang/deal_community/frament/Fragment_community.java
+++ app/src/main/java/com/first/yuliang/deal_community/frament/Fragment_community.java
@@ -21,34 +21,43 @@ import com.first.yuliang.deal_community.frament.fragment_community.Frag_communit
 public class Fragment_community  extends Fragment{
 
     private RadioGroup rg_community_tab;
+    Fragment newfragment;
+    Fragment oldfragment;
+
+    Fragment dongtai;
+    Fragment guanzhu;
+    Fragment shoucang;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.activity_community,null);
 
-        switchCommunityFragment(new Frag_community_dongtai());
+        dongtai=new Frag_community_dongtai();
+        guanzhu=new Frag_community_guanzhu();
+        shoucang=new Frag_community_shoucang();
+
+
+
+        switchCommunityFragment(dongtai);
         rg_community_tab = ((RadioGroup)view.findViewById(R.id.rg_community_tab));
         rg_community_tab.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Fragment fragment=null;
+
                 switch (checkedId){
                     case R.id.rb_community_dongtai:
-                        fragment=new Frag_community_dongtai();
+                        newfragment=dongtai;
                         break;
                     case R.id.rb_community_guanzhu:
-                        fragment=new Frag_community_guanzhu();
+                        newfragment=guanzhu;
                         break;
                     case R.id.rb_community_shoucang:
-                        fragment=new Frag_community_shoucang();
+                        newfragment=shoucang;
                         break;
                 }
-                switchCommunityFragment(fragment);
+                switchCommunityFragment(newfragment);
 
-//                FragmentManager fm= getActivity().getFragmentManager();
-//                FragmentTransaction ft= fm.beginTransaction();
-//                ft.replace(R.id.fl_community_blank,fragment).commit();
 
             }
         });
@@ -57,9 +66,25 @@ public class Fragment_community  extends Fragment{
     }
 
     private void switchCommunityFragment(Fragment fragment) {
-        FragmentManager fm= getActivity().getFragmentManager();
-        FragmentTransaction ft= fm.beginTransaction();
-        ft.replace(R.id.fl_community_blank,fragment).commit();
+        // 设置一个默认值
+        if(fragment == null){
+            fragment = dongtai;
+        }
+
+        FragmentManager fm = this.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(oldfragment!=null && !oldfragment.isHidden() && oldfragment.isAdded()){
+            ft.hide(oldfragment);
+        }
+        if(fragment.isAdded()&&fragment.isHidden()){
+            ft.show(fragment);
+        }else {
+            if (!fragment.isAdded()){
+                ft.add(R.id.fl_community_blank,fragment);
+            }
+        }
+        oldfragment=newfragment;
+        ft.commit();
     }
 
 
