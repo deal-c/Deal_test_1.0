c87f939c052aff3b980c13b6812f5208b6931c85
deal-c <2598804481@qq.com>
2016-10-20 18:21:47 +0800

    auther:张煜 text:登录注册

--- app/src/main/java/com/first/yuliang/deal_community/frament/utiles/HttpUtile.java
+++ app/src/main/java/com/first/yuliang/deal_community/frament/utiles/HttpUtile.java
@@ -5,5 +5,5 @@ package com.first.yuliang.deal_community.frament.utiles;
  */
 public class HttpUtile {
     public static final String host="http://192.168.191.1:8080/";
-
+    public static final String zy="http://192.168.191.1:8080/FourProject/";
 }
--- app/src/main/java/com/first/yuliang/deal_community/regDetailActivity.java
+++ app/src/main/java/com/first/yuliang/deal_community/regDetailActivity.java
@@ -1,21 +1,47 @@
 package com.first.yuliang.deal_community;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.first.yuliang.deal_community.frament.utiles.HttpUtile;
+import com.first.yuliang.deal_community.frament.utiles.HttpUtils;
 
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
 
-public class regDetailActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
+public class regDetailActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btn_commit;
     private EditText et_reguser;
     private EditText et_regpsd;
@@ -23,9 +49,22 @@ public class regDetailActivity extends AppCompatActivity {
     private RadioButton rb_female;
     private RadioButton btn_male;
     private EditText et_oftenplace;
-    private EditText et_recieveplace;
-    private EditText et_birthday;
 
+    private EditText et_birthday;
+    private ImageView iv_tx;
+    private static final int PHOTO_REQUEST = 1;
+    private static final int CAMERA_REQUEST = 2;
+    private static final int PHOTO_CLIP = 3;
+
+    private File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/"+
+            getPhotoFileName());
+
+    private String getPhotoFileName() {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+        return sdf.format(date)+"_"+ UUID.randomUUID() + ".png";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +79,191 @@ public class regDetailActivity extends AppCompatActivity {
         rb_female = ((RadioButton) findViewById(R.id.rb_female));
         btn_male = ((RadioButton) findViewById(R.id.rb_male));
         et_oftenplace = ((EditText) findViewById(R.id.et_oftenplace));
-        et_recieveplace = ((EditText) findViewById(R.id.et_receiveplace));
+
         et_birthday = ((EditText) findViewById(R.id.et_birthday));
+        iv_tx = ((ImageView) findViewById(R.id.iv_tx));
 
+        btn_commit.setOnClickListener(this);
+        iv_tx.setOnClickListener(this);
+    }
 
-        btn_commit.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.btn_commit:
+
+                doCommit();
+                break;
+            case R.id.iv_tx:
+
+                showSelect(v);
+
+                break;
+        }
+    }
+
+    private void showSelect(View v) {
+        View contentView= LayoutInflater.from(this).inflate(R.layout.activity_select_item,null);
+        Button btn_camera=((Button) contentView.findViewById(R.id.btn_camera));
+        Button btn_choose=((Button) contentView.findViewById(R.id.btn_choose));
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RequestParams params=new RequestParams("http://10.40.5.21:8080/FourProject/servlet/RegAppServlet");
-                params.addBodyParameter("regusername",et_reguser.getText().toString().trim());
-                params.addBodyParameter("reguserpsd",et_regpsd.getText().toString().trim());
-                params.addBodyParameter("userSex",rb_female.isChecked()?"false":"true");
-                params.addBodyParameter("birthday",et_birthday.getText().toString().trim());
-                params.addBodyParameter("userAddress_s",et_oftenplace.getText().toString().trim());
-                params.addBodyParameter("userAddress_c",et_recieveplace.getText().toString().trim());
+                takePhoto();
+            }
+        });
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPicFromPhoto();
+            }
+        });
+
+        final PopupWindow popupWindow = new PopupWindow(contentView,
+                ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+
+        popupWindow.setTouchable(true);
+
+        popupWindow.setTouchInterceptor(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                return false;
+
+            }
+        });
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
+
+        popupWindow.showAtLocation(v, Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0, 0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        switch (requestCode) {
+            case CAMERA_REQUEST:
+                switch (resultCode) {
+                    case -1://-1表示拍照成功  固定
+                        System.out.println("CAMERA_REQUEST"+file.getAbsolutePath());
+                        if (file.exists()) {
+                            photoClip(Uri.fromFile(file));
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case PHOTO_REQUEST:
+                if (data != null) {
+                    photoClip(data.getData());
+
+                }
+                break;
+            case PHOTO_CLIP:
+                if (data != null) {
+                    Bundle extras = data.getExtras();
+                    if (extras != null) {
+                        Log.w("test", "data");
+                        Bitmap photo = extras.getParcelable("data");
+                        saveImageToGallery(getApplication(),photo);//保存bitmap到本地
+                        iv_tx.setImageBitmap(photo);
+
+
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void saveImageToGallery(Context context, Bitmap bmp) {
+
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    file.getAbsolutePath(), file.getName(), null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 最后通知图库更新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + file.getAbsolutePath())));
+    }
+
+    private void photoClip(Uri uri) {
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // 下面这个crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, PHOTO_CLIP);
+    }
+
+    private void takePhoto() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 下面这句指定调用相机拍照后的照片存储的路径
+        // System.out.println("getPicFromCamera==========="+file.getAbsolutePath());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+
+    private void getPicFromPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*");
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
+
+
+    private void doCommit() {
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                sendImage();
+            }
+
+            private void sendImage() {
+
+                RequestParams params = new RequestParams(HttpUtils.host+"/upload");
+                params.addBodyParameter("file",file);
                 x.http().post(params, new Callback.CommonCallback<String>() {
+
                     @Override
                     public void onSuccess(String result) {
-
-
-                        Toast.makeText(regDetailActivity.this,result,Toast.LENGTH_SHORT).show();
-                        Intent intent=new Intent(regDetailActivity.this, mainActivity.class);
-                        startActivity(intent);
+                        Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -80,8 +282,60 @@ public class regDetailActivity extends AppCompatActivity {
                     }
                 });
             }
+        }).start();
+        RequestParams params=new RequestParams(HttpUtile.zy+"/servlet/RegAppServlet");
+        try {
+            params.addBodyParameter("regusername", URLEncoder.encode(et_reguser.getText().toString().trim(),"utf-8"));
+            params.addBodyParameter("reguserpsd",et_regpsd.getText().toString().trim());
+            params.addBodyParameter("userSex",rb_female.isChecked()?"false":"true");
+            params.addBodyParameter("birthday",et_birthday.getText().toString().trim());
+            params.addBodyParameter("userAddress_s", URLEncoder.encode(et_oftenplace.getText().toString().trim(),"utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+        x.http().post(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+
+
+                SharedPreferences preference=getSharedPreferences("shared_loginn_info", Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit=preference.edit();
+
+
+
+                if(Integer.parseInt(result.trim())!=0) {
+                    Toast.makeText(regDetailActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+
+                    edit.putInt("count",1);
+                    edit.putInt("id",Integer.parseInt(result));
+                    edit.commit();
+                    Intent intent = new Intent(regDetailActivity.this, mainActivity.class);
+                    startActivity(intent);
+                    regDetailActivity.this.finish();
+                }else
+                {
+                    Toast.makeText(regDetailActivity.this,"注册失败",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
         });
-
-
     }
+
+
 }
