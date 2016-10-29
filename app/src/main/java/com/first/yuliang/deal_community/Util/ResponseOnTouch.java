da9785a2c89e9893d45b8c68dcde21e465596b73
deal-c <2598804481@qq.com>
2016-10-29 09:33:46 +0800

    author:sun desc:完善搜索结果

---
+++ app/src/main/java/com/first/yuliang/deal_community/Util/ResponseOnTouch.java
@@ -0,0 +1,9 @@
+package com.first.yuliang.deal_community.Util;
+
+/**
+ * Created by vanex on 2016/10/28.
+ */
+
+public interface ResponseOnTouch {
+        public void onTouchResponse(int volume);
+}
